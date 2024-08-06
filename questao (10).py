59ef8e61bf8630dcf5b8fc3eeaeb3803d50ff67f
Evelynsuzarte <evelynsuzarte@hotmail.com>
2024-08-05 23:50:04 -0300

    organização do repositório

---
+++ questao (10).py
@@ -0,0 +1,21 @@
+"""10. Faça um Programa que utilize 4 variáveis como preferir e no final print
+uma mensagem amigável utilizando as variáveis criadas.
+
+Exemplos de variáveis: nome, idade, lugar, profissão ....
+Exemplo de retorno: Olá Maria, prazer te conhecer. Sou de São Paulo
+também e estou migrando de área.
+
+Lembrando que para o retorno vamos usar print com as variáveis
+criadas e este texto é somente um exemplo, utilizem a criatividade."""
+
+# Variáveis
+nome = "Beatriz"
+idade = 26
+cidade = "São Paulo"
+profissao = "Ciência de dados"
+
+# Formando a mensagem amigável
+mensagem = "Olá {nome}, é um prazer te conhecer! Você tem {idade} anos e mora em {cidade}, não é mesmo? Parabéns por estar se tornando uma profissional de {profissao}. Desejo muito sucesso nessa nova jornada de desenvolvimento!"
+
+# Exibindo a mensagem
+print(mensagem)
